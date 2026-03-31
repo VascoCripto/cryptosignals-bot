6fd6271bf1b35cf43477f58cefd48074b02a6c9e
VascoCripto <aposentadoria.fabricio@gmail.com>
2026-03-31 16:14:36 -0300

    Correção da leitura do JSON do TradingView

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const axios = require('axios')
 
 const app = express()
-app.use(express.json())
+app.use(express.json({ type: '*/*' }))
 
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID
